a8ceccded45a12d7ef5c7eb538c62df6477a5591
Boncroix <boncroix@gmail.com>
2023-10-03 21:18:28 +0200

    feat: ya sabemos borrar movimientos

--- balance/models.py
+++ balance/models.py
@@ -43,3 +43,23 @@ class DBManager:
 
         # 6. Devolver los resultados
         return self.registros
+
+    def borrar(self, id):
+        '''
+        DELETE FROM movimientos WHERE id=?
+        '''
+        sql = 'DELETE FROM movimientos WHERE id=?'
+        conexion = sqlite3.connect(self.ruta)
+        cursor = conexion.cursor()
+
+        resultado = False
+        try:
+            cursor.execute(sql, (id,))
+            conexion.commit()
+            resultado = True
+        except:
+            conexion.rollback()
+            resultado = False
+
+        conexion.close()
+        return resultado
--- balance/views.py
+++ balance/views.py
@@ -10,3 +10,15 @@ def home():
     sql = 'SELECT id, fecha, concepto, tipo, cantidad FROM movimientos'
     movimientos = db.consultaSQL(sql)
     return render_template('inicio.html', movs=movimientos)
+
+# - Funcion borrar -- DONE
+# - Operar con la BD
+# - Botón de borrado en cada movimiento
+# - Plantilla con el resultado -- DONE
+
+
+@app.route('/borrar/<int:id>')
+def eliminar(id):
+    db = DBManager(RUTA)
+    ha_ido_bien = db.borrar(id)
+    return render_template('borrado.html', resultado=ha_ido_bien)
