c57041807223bd8de1757bed72d9329dccec0030
José Bueno <boncroix@gmail.com>
2023-10-05 17:41:27 +0200

    feat: otro metodo de rellenar formulario editar

--- balance/views.py
+++ balance/views.py
@@ -1,4 +1,4 @@
-
+from datetime import date
 from flask import render_template, request
 
 from . import RUTA, app
@@ -32,5 +32,17 @@ def actualizar(id):
         db = DBManager(RUTA)
         movimiento = db.editar(id)
         formulario = MovimientoForm(data=movimiento)
+        return render_template('form_movimiento.html', form=formulario)
+    return f'TODO: tratar el método POST para actualizar el movimiento {id}'
+
+@app.route('/editar1/<int:id>')
+def editar(id):
+    if request.method == 'GET':
+        db = DBManager(RUTA)
+        sql = f'SELECT id, fecha, concepto, tipo, cantidad FROM movimientos WHERE id={id}'
+        dato = db.consultaSQL(sql)
+        dato = dato[0]
+        dato['fecha'] = date.fromisoformat(dato['fecha'])
+        formulario = MovimientoForm(data=dato)
         return render_template('form_movimiento.html', form=formulario)
     return f'TODO: tratar el método POST para actualizar el movimiento {id}'
