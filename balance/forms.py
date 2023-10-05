57d21edf9913296a810cafba4e1d33fb97d0066a
Boncroix <boncroix@gmail.com>
2023-10-05 21:38:51 +0200

    fix: corregir error en la validación de datos

--- balance/forms.py
+++ balance/forms.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import (DateField, FloatField, HiddenField, RadioField,
+from wtforms import (DateField, DecimalField, HiddenField, RadioField,
                      StringField, SubmitField)
 from wtforms.validators import DataRequired, NumberRange
 
 
 class MovimientoForm(FlaskForm):
     id = HiddenField()
-    fecha = DateField('Fecha', validators=(DataRequired()))
-    concepto = StringField('Concepto', validators=(DataRequired()))
+    fecha = DateField('Fecha', validators=[DataRequired()])
+    concepto = StringField('Concepto', validators=[DataRequired()])
     tipo = RadioField(
-        choices=[('I', 'Ingreso'), ('G', 'Gasto')], validators=DataRequired())
-    cantidad = FloatField('Cantidad', validators=[
-        DataRequired(),
-        NumberRange(min=0.1, message='No se permiten cantidades inferiores a 10 céntimos')])
+        choices=[('I', 'Ingreso'), ('G', 'Gasto')], validators=[DataRequired(message='Debes especificar si es un gasto o un ingreso')])
+    cantidad = DecimalField(
+        'Cantidad', places=2, validators=[
+            DataRequired(),
+            NumberRange(min=0.1, message='No se permiten cantidades inferiores a 10 céntimos')])
 
     submit = SubmitField('Guardar')
